89bcd065a95b712ea2b9f2b17d862c8045acb9ce
Alisa Perepelova <alice-lilo@mail.ru>
2025-04-23 21:54:10 +0300

    listener added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,22 +1,26 @@
 package tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverProvider;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import config.WebDriverProvider;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 public class TestBase {
   @BeforeEach
-  void setUp()
-  {
+  void setUp() {
     WebDriverProvider.setUp();
+  }
+
+  @BeforeEach
+  void addSelenideListener() {
     SelenideLogger.addListener("allure", new AllureSelenide());
   }
+
   @AfterEach
   public void tearDown() {
     Attach.screenshotAs("Last screen");
